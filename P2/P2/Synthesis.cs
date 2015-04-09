8a7873a754dbcb4d4fd57f973297782e6031bf71
Arne Rasmussen <Gedesneglen@gmail.com>
2015-04-09 13:22:36 +0200

    Made thread

--- P2/P2/Synthesis.cs
+++ P2/P2/Synthesis.cs
@@ -3,28 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace P2
 {
     //This class handles the time of the simulation and presents features to stop, fastforward and convert time from realtime to virtualtime
+    /* The constructor for TimeHandler
+     * Variable
+     * Variable
+     * Output
+     */
     public class Synthesis
     {
         private DataHandler simulationData;
         private Model SimulationModel;
-        /* The constructor for TimeHandler
-         * Variable
-         * Variable
-         * Output
-         */
-        public Synthesis()
+        bool catalyst;
+        Thread Simulation;
+
+        public Synthesis(double iTemperature, double nHydrogen, double nNitrogen, double nAmmonia, bool catalyst, double time, double volume)
         {
             simulationData = new DataHandler();
-            SimulationModel = new Model();
+            SimulationModel = new Model(iTemperature, nHydrogen, nNitrogen, nAmmonia, catalyst, time, volume);
+            Simulation = new Thread(run);
+
+        }
+
+        public void run()
+        {
+
         }
 
         public void start()
         {
-
+            Simulation.Start();
         }
 
         public void stop()
