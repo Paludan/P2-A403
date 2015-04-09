d8d89ad00220a02d5fcc9378ed04a6ea93bf0726
Arne Rasmussen <Gedesneglen@gmail.com>
2015-04-09 13:45:51 +0200

    Property

--- P2/P2/Synthesis.cs
+++ P2/P2/Synthesis.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Timers;
 
 namespace P2
 {
@@ -17,20 +18,31 @@ namespace P2
     {
         private DataHandler simulationData;
         private Model SimulationModel;
-        bool catalyst;
+        bool catalyst, running = true;
         Thread Simulation;
+        System.Timers.Timer timer;
+
+        public double interval
+        {
+            get { return timer.Interval; }
+            set { timer.Interval = value;}
+        }
 
         public Synthesis(double iTemperature, double nHydrogen, double nNitrogen, double nAmmonia, bool catalyst, double time, double volume)
         {
             simulationData = new DataHandler();
             SimulationModel = new Model(iTemperature, nHydrogen, nNitrogen, nAmmonia, catalyst, time, volume);
-            Simulation = new Thread(run);
+            timer = new System.Timers.Timer(500);
 
         }
 
         public void run()
         {
+            Timer
+            while (running)
+            {
 
+            }
         }
 
         public void start()
