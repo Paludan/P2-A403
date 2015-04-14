348e431333bcc85527206b3ceee6b1c903628c20
Morell <tmorel14@student.aau.dk>
2015-04-14 11:10:54 +0200

    constasnts

---
+++ P2/P2/P2GraphPanel/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Drawing;
+
+namespace P2Graph
+{
+	public static class Constants
+	{
+		public static readonly float xOffset = 0.04f;
+		public static readonly float yOffset = 0.95f;
+		public static readonly float endOffset = 0.95f;
+		public static float yPixelScale = 0;
+		public static float xPixelScale = 0;
+	}
+}
+
