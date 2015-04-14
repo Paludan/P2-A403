d567bf1e99805dbea30146406a93397fbd850608
Arne Rasmussen <Gedesneglen@gmail.com>
2015-04-14 09:25:49 +0200

    Made comments and figured out the elapsed event

--- P2/P2/Synthesis.cs
+++ P2/P2/Synthesis.cs
@@ -8,82 +8,91 @@ using System.Timers;
 
 namespace P2
 {
-    //This class handles the time of the simulation and presents features to stop, fastforward and convert time from realtime to virtualtime
-    /* The constructor for TimeHandler
-     * Variable
-     * Variable
-     * Output
-     */
+    /// <summary>
+    /// This class handles the time of the simulation and presents features to stop, 
+    /// fastforward and convert time from realtime to virtualtime
+    /// </summary>
     public class Synthesis
     {
         DataHandler simulationData;
         Model SimulationModel;
         bool running = false;
-        Thread Simulation; //This variable may or may not be used in the final version.
+        Thread Simulation; //A thread to ensure multitasking.
         System.Timers.Timer timer;//There are other classes called Timer so we must specify the path when initializing.
         public DataPoint currentData = new DataPoint(0,0,0,0,0,0,false);
         double Scale = 1.0; // this variable will decide at what scale the time runs. by making this 2.0, the virtual time elapsed
                             // when calculating a new datapoint will be double the actual alapsed time.
-        /*
-         * With this property, the user of the class can specify the time interval between datapoints being taken.
-         */
+
+        /// <summary>
+        /// With this property, the user of the class can specify the time interval between datapoints being taken.
+        /// </summary>
         public double interval
         {
             get { return timer.Interval; }
             set { timer.Interval = value;}
         }
 
-        /*This is the constructor for the class. It initializes the datahandler, model and timer.
-         */
+        /// <summary>
+        /// This is the constructor for the class. It initializes the datahandler, model and timer.
+        /// </summary>
         public Synthesis()
         {
             simulationData = new DataHandler();
             SimulationModel = new Model(currentData);
             timer = new System.Timers.Timer(500);
+            timer.Elapsed += this.Update;
 
         }
         /*This function starts a new thread that runs the timer and controls the generation of data points
          */
+        /// <summary>
+        /// This function starts a new thread that runs the timer and controls the generation of data points
+        /// </summary>
         public void run()
         {
             timer.Start();
         }
 
-        /*this method is invoked by the caller when the simulation needs to start.
-         */
+        /// <summary>
+        /// this method is invoked by the caller when the simulation needs to start.
+        /// </summary>
         public void start()
         {
             running = true;
             Simulation.Start();
         }
 
-        /*
-         * This method will add a new datapoint using the 
-         */
-        public void Update(double deltaTime){
+        /// <summary>
+        /// This method will add a new datapoint using the
+        /// </summary>
+        /// <param name="source">The object that invoked the event</param>
+        /// <param name="e">arguments dunno</param>
+        public void Update(Object source, ElapsedEventArgs e)
+        {
             currentData.time+=interval;
 
-            simulationData.addDataPoint( SimulationModel.calculateDataPoint(deltaTime) );
+            simulationData.addDataPoint( SimulationModel.calculateDataPoint(interval) );
         }
 
-        /*This method will stop or pause the simulation of this synthesis until it gets started again.
-         */
+        /// <summary>
+        /// This method will stop or pause the simulation of this synthesis until it gets started again.
+        /// </summary>
         public void stop()
         {
 
         }
 
-        /*
-         * Not sure if this function will be in use yet.
-         */
+        /// <summary>
+        /// Not sure if this function will be in use yet.
+        /// </summary>
         public void search()
         {
 
         }
 
-        /*
-         * Not sure if this function will be in use yet.
-         */
+        /// <summary>
+        /// Not sure if this function will be in use yet.
+        /// </summary>
         public void fastforward()
         {
 
