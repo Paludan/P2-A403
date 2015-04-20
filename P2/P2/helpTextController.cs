2e31ddb2d4512fb9eeb6b1ff48a89b53313a5795
Jonas Stolberg <jStolberg>
2015-04-20 10:13:39 +0200

    mostly fixed
    except on cycle it doesn't show the first set of instructions.?? I dunno

--- P2/P2/helpTextController.cs
+++ P2/P2/helpTextController.cs
@@ -17,7 +17,7 @@ namespace P2
         {
             helpText = SaveLoadTools.loadText("vejledning.txt");
             currentLine = -1;
-            if (helpText == null) { helpText = new String[1]; helpText[0] = "Ingen vejledning fundet."; }
+            if (helpText == null) { helpText = new String[2]; helpText[0] = "Ingen vejledning fundet."; helpText[1] = "opret 'vejledning.txt' i program mappen."; }
         }
 
         /// <summary>
@@ -28,9 +28,9 @@ namespace P2
         {
             currentLine++;
             String[] output = new String[2];
-            if (currentLine > helpText.Length) { currentLine = 0; }
-            else if (currentLine == helpText.Length) { output[0] = helpText.ElementAt(currentLine); output[1] = "Færdig. Klik 'Videre' igen for at gentage vejledning."; }
-            else { output[0] = helpText.ElementAt(currentLine); output[1] = helpText.ElementAt(currentLine); }
+            if (currentLine >= helpText.Length) { currentLine = 0; }
+            else if (currentLine == helpText.Length - 1) { output[0] = helpText.ElementAt(currentLine); output[1] = "Færdig. Klik 'Videre' igen for at gentage vejledning."; }
+            else { output[0] = helpText.ElementAt(currentLine); output[1] = helpText.ElementAt(currentLine + 1); }
             return output;
         }
     }
