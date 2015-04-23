5f7635c0b8b23731f6c5eba080dabd2ccede03e3
Morell <tmorel14@student.aau.dk>
2015-04-23 21:18:35 +0200

    forsøg på ligningsløsning

---
+++ Equation/AmmoniaTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AmmoniaTest
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			AmmoniaCalc ac = new AmmoniaCalc ();
+
+			double solution = ac.solveQuadricEquation (200, 200, 12, 0.5);
+			Console.WriteLine ("\nSolution: " + solution);
+		}
+	}
+}
