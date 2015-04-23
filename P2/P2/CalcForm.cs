e4b80083d0723d4f30ffadbc394e04fc002e39a1
Lee Paludan <leepaludan@gmail.com>
2015-04-23 11:50:25 +0200

    removed useless spaces

--- P2/P2/CalcForm.cs
+++ P2/P2/CalcForm.cs
@@ -34,55 +34,72 @@ namespace P2
 
             InitializeComponent();
             UpdateData();
-
-            cm.MenuItems.Add("Nitrogen");
-            cm.MenuItems.Add("Hydrogen");
-            cm.MenuItems.Add("Ammoniak");
-
-            cm.MenuItems[0].Click += menuItem1_ItemClick;
-			cm.MenuItems[1].Click +=  menuItem2_ItemClick;
-			cm.MenuItems[2].Click +=  menuItem3_ItemClick;
+            setupContextMenu();
 
             this.idealGasTitle.ContextMenu = cm;
         }
 
 		public void UpdateData(){
-            
-            this.idealGasTitle.Text = "Idealgasligningen " + "(nitrogen)";
-			this.temperature.Text = _synth.currentData.temperature.ToString();
-			this.molarmass.Text = _synth.currentData.nNitrogen.ToString();
-			this.gasConst.Text = gasConstant.ToString ();
-			this.Volume.Text = volume.ToString ();
-			this.partialpressure.Text = ((_synth.currentData.nNitrogen * _synth.currentData.temperature * gasConstant)
-                / volume).ToString("#.##");
 
-            /* EquiFrac */
+
+            setupStartIdealGasLaw();
+            calcEquiFrac();
+            calcRRConst();
+        
+		}
+
+        private void setupContextMenu()
+        {
+            cm.MenuItems.Add("Nitrogen");
+            cm.MenuItems.Add("Hydrogen");
+            cm.MenuItems.Add("Ammoniak");
+
+            cm.MenuItems[0].Click += menuItem1_ItemClick;
+            cm.MenuItems[1].Click += menuItem2_ItemClick;
+            cm.MenuItems[2].Click += menuItem3_ItemClick;
+        }
+
+        private void setupStartIdealGasLaw()
+        {
+            this.idealGasTitle.Text = "Idealgasligningen " + "(nitrogen)";
+            this.temperature.Text = _synth.currentData.temperature.ToString();
+            this.molarmass.Text = _synth.currentData.nNitrogen.ToString();
+            this.gasConst.Text = gasConstant.ToString();
+            this.Volume.Text = volume.ToString();
+            this.partialpressure.Text = ((_synth.currentData.nNitrogen * _synth.currentData.temperature * gasConstant)
+                / volume).ToString("#.##");
+        }
+
+        private void calcEquiFrac()
+        {
             this.equiFracResult.Text = string.Format("{0:N7}", (Math.Pow(((this._synth.currentData.nAmmonia * this._synth.currentData.temperature * gasConstant)
-                    / volume), 2) / (Math.Pow(((this._synth.currentData.nNitrogen * this._synth.currentData.temperature * gasConstant)
-                    / volume), 2) * Math.Pow(((this._synth.currentData.nHydrogen * this._synth.currentData.temperature * gasConstant)
-                    / volume), 2))));
+        / volume), 2) / (Math.Pow(((this._synth.currentData.nNitrogen * this._synth.currentData.temperature * gasConstant)
+        / volume), 2) * Math.Pow(((this._synth.currentData.nHydrogen * this._synth.currentData.temperature * gasConstant)
+        / volume), 2))));
 
             this.equiTitle.Text = "Ligevægtsbrøken: " + "Y = " + string.Format("{0:N7}", (Math.Pow(Math.E, -(entalpi
                     / (this._synth.currentData.temperature * gasConstant)) + (entropi / gasConstant))));
 
-            this.pAmmoniaEqui.Text = string.Format("{0:N3}", Math.Pow(((this._synth.currentData.nAmmonia * this._synth.currentData.temperature * gasConstant) 
+            this.pAmmoniaEqui.Text = string.Format("{0:N3}", Math.Pow(((this._synth.currentData.nAmmonia * this._synth.currentData.temperature * gasConstant)
                     / volume), 2));
 
-            this.pNitrogenEqui.Text =  string.Format("{0:N3}", Math.Pow(((this._synth.currentData.nNitrogen * this._synth.currentData.temperature * gasConstant)
+            this.pNitrogenEqui.Text = string.Format("{0:N3}", Math.Pow(((this._synth.currentData.nNitrogen * this._synth.currentData.temperature * gasConstant)
                    / volume), 2));
 
             this.pHydrogenEqui.Text = string.Format("{0:N3}", (Math.Pow(((this._synth.currentData.nHydrogen * this._synth.currentData.temperature * gasConstant)
                     / volume), 2)));
+        }
 
-            /* RRConst */
+        private void calcRRConst()
+        {
             this.preExponentialFactor.Text = preExpontentialFactor.ToString();
             this.activationEnergy.Text = (this._synth.currentData.catalyst == true) ? 60.ToString() : 1100.ToString();
             this.gasConstRR.Text = gasConstant.ToString();
             this.temperatureRR.Text = this._synth.currentData.temperature.ToString();
-            this.RRConst.Text = string.Format("{0:N7}", preExpontentialFactor 
-                    * Math.Pow(Math.E, ( (this._synth.currentData.catalyst == true) ? 60 : 1100) 
-                    / (gasConstant * this._synth.currentData.temperature)));          
-		}
+            this.RRConst.Text = string.Format("{0:N7}", preExpontentialFactor
+                    * Math.Pow(Math.E, ((this._synth.currentData.catalyst == true) ? 60 : 1100)
+                    / (gasConstant * this._synth.currentData.temperature)));  
+        }
 
 		private void menuItem1_ItemClick (object sender, System.EventArgs e)
 		{
