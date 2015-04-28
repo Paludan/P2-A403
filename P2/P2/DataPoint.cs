de655f0cc878a14b8da29c49df9bcc3863f00454
Esben Kirkegaard <esben411@hotmail.com>
2015-04-28 08:36:17 +0200

    Fjernet tryk i datapoints

--- P2/P2/DataPoint.cs
+++ P2/P2/DataPoint.cs
@@ -11,19 +11,18 @@ namespace P2
     // This struct contains the simulation data at a specific time
     public struct DataPoint : ISerializable
     {
-        public double temperature, pressure, time;
+        public double temperature, time;
         private double _nAmmonia, _nHydrogen, _nNitrogen;
         public bool catalyst;
         /// <summary>
         /// Constructs a datapoint from a set of given variables
         /// </summary>
-        public DataPoint(double ammonia, double hydrogen, double nitrogen, double inputTemperature, double inputPressure, double inputTime, bool inputCatalyst)
+        public DataPoint(double ammonia, double hydrogen, double nitrogen, double inputTemperature, double inputTime, bool inputCatalyst)
         {
             this._nAmmonia = ammonia;
             this._nHydrogen = hydrogen;
             this._nNitrogen = nitrogen;
             this.temperature = inputTemperature;
-            this.pressure = inputPressure;
             this.time = inputTime;
             this.catalyst = inputCatalyst;
         }
@@ -50,14 +49,13 @@ namespace P2
             this._nHydrogen = oldData.nHydrogen;
             this._nNitrogen = oldData.nNitrogen;
             this.temperature = oldData.temperature;
-            this.pressure = oldData.pressure;
             this.time = oldData.time;
             this.catalyst = oldData.catalyst;
         }
         // Allows for easy printing of a datapoint
         public override string ToString()
         {
-            return String.Format("{0} {1} {2} {3} {4} {5} {6}", nAmmonia, nHydrogen, nNitrogen, temperature, pressure, time, catalyst);
+            return String.Format("{0} {1} {2} {3} {4} {5} {6}", nAmmonia, nHydrogen, nNitrogen, temperature, time, catalyst);
         }
         #region ISerializable implementation
 
@@ -67,7 +65,6 @@ namespace P2
             info.AddValue("Hydrogen", this._nHydrogen);
             info.AddValue("Nitrogen", this._nNitrogen);
             info.AddValue("Temperature", this.temperature);
-            info.AddValue("Pressure", this.pressure);
             info.AddValue("Time", this.time);
             info.AddValue("Catalyst", this.catalyst);
         }
@@ -77,7 +74,6 @@ namespace P2
             this._nHydrogen = (double)info.GetValue("Hydrogen", typeof(double));
             this._nNitrogen = (double)info.GetValue("Nitrogen", typeof(double));
             this.temperature = (double)info.GetValue("Temperature", typeof(double));
-            this.pressure = (double)info.GetValue("Pressure", typeof(double));
             this.time = (double)info.GetValue("Time", typeof(double));
             this.catalyst = (bool)info.GetValue("Catalyst", typeof(bool));
         }
