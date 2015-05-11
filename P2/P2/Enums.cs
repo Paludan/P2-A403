dd40cb781100d27f8f7ad70c3b78ca432a9a3542
Esben Kirkegaard <esben411@hotmail.com>
2015-05-11 11:17:35 +0200

    Fjernet merge halløj

--- P2/P2/Enums.cs
+++ P2/P2/Enums.cs
@@ -6,13 +6,9 @@ using System.Threading.Tasks;
 
 namespace P2
 {
-<<<<<<< HEAD
-    //Enumerations used by the graph
-=======
 	/// <summary>
 	/// Enums for colors and graph content
 	/// </summary>
->>>>>>> 8eac1fbbc727b242f84fe92979c84a39c8ffa960
     public enum graph { temperature, pressure, ammonia, hydrogen, nitrogen }
     enum colors { Rød, Grøn, Blå, Lilla, Sort, Gennemsigtig }
 }
